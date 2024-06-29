0fd69b0f775351861b304399d1578d51d220c017
Damon <126731021+damon314159@users.noreply.github.com>
2024-06-29 11:20:45 +0100

    Create types for a services DI pattern

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,17 @@
-import type { GetClient, Query } from './db/index.js'
+import type {
+  CreateService,
+  CreateServiceParameters,
+  Dependencies,
+  Service,
+} from './services/index.js'
+import type { Accessors, GetClient, Query } from './db/index.js'
 
-export { GetClient, Query }
+export {
+  Accessors,
+  CreateService,
+  CreateServiceParameters,
+  Dependencies,
+  GetClient,
+  Query,
+  Service,
+}
---
+++ src/types/services/index.d.ts
@@ -0,0 +1,5 @@
+import type { CreateService, CreateServiceParameters } from './CreateService.js'
+import type { Dependencies } from './Dependencies.js'
+import type { Service } from './Service.js'
+
+export { CreateService, CreateServiceParameters, Dependencies, Service }
