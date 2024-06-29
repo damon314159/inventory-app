defad3b0f868b5c10271cbd11642657846359aab
Damon <126731021+damon314159@users.noreply.github.com>
2024-06-29 12:37:40 +0100

    Create types for both item and category models

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,17 +1,20 @@
+import type { Accessors, GetClient, Query } from './db/index.js'
+import type { Category, Item } from './models/index.js'
 import type {
   CreateService,
   CreateServiceParameters,
   Dependencies,
   Service,
 } from './services/index.js'
-import type { Accessors, GetClient, Query } from './db/index.js'
 
 export {
   Accessors,
+  Category,
   CreateService,
   CreateServiceParameters,
   Dependencies,
   GetClient,
+  Item,
   Query,
   Service,
 }
