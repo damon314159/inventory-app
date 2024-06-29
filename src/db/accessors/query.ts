15a5be01d27cf4b09460195c7c5b6c09888ed267
Damon <126731021+damon314159@users.noreply.github.com>
2024-06-29 12:26:19 +0100

    Fix query type not having params be optional

--- src/db/accessors/query.ts
+++ src/db/accessors/query.ts
@@ -3,7 +3,7 @@ import type { Query } from '../../types/index.js'
 
 const createQuery =
   (pool: Pool): Query =>
-  (queryText: string, params: unknown[]): Promise<QueryResult> =>
+  (queryText: string, params?: unknown[]): Promise<QueryResult> =>
     pool.query(queryText, params)
 
 export default createQuery
