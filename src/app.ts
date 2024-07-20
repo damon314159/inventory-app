ff668671b4ea6aa74991973cd070df6728ef918d
Damon <126731021+damon314159@users.noreply.github.com>
2024-07-20 15:55:40 +0100

    Implement method override middleware to app file

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import express, {
 import 'express-async-handler'
 import helmet from 'helmet'
 import createError from 'http-errors'
+import methodOverride from 'method-override'
 import logger from 'morgan'
 import indexRouter from './routes/index.js'
 import usersRouter from './routes/users.js'
@@ -44,6 +45,7 @@ app.use(
 app.set('view engine', 'ejs')
 app.set('views', resolve(__dirname, 'views'))
 
+app.use(methodOverride('_method'))
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
