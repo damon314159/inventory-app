e1a3a663c6d9f9907cb6d34ebf5bd1a43e8166d4
Damon <126731021+damon314159@users.noreply.github.com>
2024-07-22 03:11:30 +0100

    Split index and about handlers into own file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,10 @@
-import express, { type RequestHandler } from 'express'
+import { Router } from 'express'
+import { aboutController, indexController } from '../controllers/index.js'
 
 // Instantiate a new router
-const router = express.Router()
+const router = Router()
 
-/* GET home page. */
-const indexController: RequestHandler = (_req, res): void => {
-  res.render('index', { totalItems: 1500, totalLowStock: 20 })
-}
 router.get('/', indexController)
-
-const aboutController: RequestHandler = (_req, res): void => {
-  res.render('about')
-}
 router.get('/about', aboutController)
 
 export default router
